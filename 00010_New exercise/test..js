83a18f7cc1256d8aa8408ee1701f9a7c2bddb1b3
franco@mumuki.org <franco@mumuki.org>
2020-05-13 14:04:50 +0000

    Mumuki Export on 2020-05-13 14:04:50 +0000

--- 00010_New exercise/test..js
+++ 00010_New exercise/test..js
@@ -1,5 +1,5 @@
 describe("Group description", function() {
-  it(("Sample description" + "hola"), function() {
+  it(("Sample description" + (new Date()).toString()  +"hola"), function() {
     assert(true)
   })
 })
