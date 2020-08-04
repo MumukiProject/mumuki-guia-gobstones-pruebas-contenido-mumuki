6af9e4d436540ee6ae20fb8e9ccb30a90b5009f0
franco@mumuki.org <franco@mumuki.org>
2020-08-04 19:37:37 +0000

    Mumuki Export on 2020-08-04 19:37:37 +0000

--- 00013_¡Rompecabezas!/test.js
+++ 00013_¡Rompecabezas!/test.js
@@ -1,4 +1,4 @@
 
 Muzzle.canvasHeight = 400
 Muzzle.canvasWidth = 400
-Muzzle.basic(3, 2, 'https://flbulgarelli.github.io/headbreaker/static/berni.jpg');
+Muzzle.basic(5, 6, 'https://flbulgarelli.github.io/headbreaker/static/berni.jpg');
