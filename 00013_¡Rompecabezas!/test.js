a46a0f8fa3f235cd0795189dfe56d33a8af0070b
franco@mumuki.org <franco@mumuki.org>
2020-08-04 20:14:25 +0000

    Mumuki Export on 2020-08-04 20:14:25 +0000

--- 00013_¡Rompecabezas!/test.js
+++ 00013_¡Rompecabezas!/test.js
@@ -1,3 +1,11 @@
+// estas tres lineas son opcionales
+Muzzle.canvasHeight = 600;
+Muzzle.canvasWidth = 1200;
+Muzzle.pieceSize = 200;
+
+const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-alternativa-kids/master/assets/attires/';
+
+
 // caso "choice"
 Muzzle.match([
   `${baseUrl}/va_vacio.png`
