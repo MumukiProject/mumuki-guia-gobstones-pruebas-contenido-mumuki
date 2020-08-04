a96ba3a1e4808d9f13d3c2fcaf0ee7521bc6bacb
franco@mumuki.org <franco@mumuki.org>
2020-08-04 19:52:25 +0000

    Mumuki Export on 2020-08-04 19:52:25 +0000

--- 00013_¡Rompecabezas!/test.js
+++ 00013_¡Rompecabezas!/test.js
@@ -3,7 +3,6 @@ Muzzle.canvasWidth = 1200;
 Muzzle.pieceSize = 200;
 const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-alternativa-kids/master/assets/attires/';
 
-// with left and right pieces
 Muzzle.match([
   `${baseUrl}/va_vacio.png`,
   `${baseUrl}/cu_vacio.png`,
@@ -12,6 +11,8 @@ Muzzle.match([
   `${baseUrl}/va_fru.png`,
   `${baseUrl}/cu_vai.png`,
   `${baseUrl}/chips_mucho.png`,
+], [
+  `${baseUrl}/choc_mitad_vacio2.png`
 ]);
 
 
