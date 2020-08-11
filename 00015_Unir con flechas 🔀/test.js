372bd529ce92cc725d7a28ce7fafd87028360de6
rocio@mumuki.org <rocio@mumuki.org>
2020-08-11 21:32:34 +0000

    Mumuki Export on 2020-08-11 21:32:34 +0000

--- 00015_Unir con flechas 🔀/test.js
+++ 00015_Unir con flechas 🔀/test.js
@@ -18,5 +18,9 @@ Muzzle.match([
    `${baseUrl}/chips_mucho.png`,
  ], [
    `${baseUrl}/choc_mitad_vacio2.png` // opcional
- ]);
+ ]).then(canvas => {
+          canvas.shuffle(0.4);
+          canvas.puzzle.pieces.filter(it => !it.metadata.left).forEach(it => it.translate(400, 0))
+          canvas.draw();
+        });
 
