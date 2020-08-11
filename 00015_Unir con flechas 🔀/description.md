dd506b111ff3ce619e187ab9f9d76c030822d071
rocio@mumuki.org <rocio@mumuki.org>
2020-08-11 15:32:01 +0000

    Mumuki Export on 2020-08-11 15:32:01 +0000

---
+++ 00015_Unir con flechas 🔀/description.md
@@ -0,0 +1,3 @@
+<img src="https://tenor.com/es/ver/cool-ice-cream-happy-gif-13278057" alt="" width="auto" height="auto">
+
+> UNÍ LOS PARES CORRESPONDIENTES :icecream::shaved_ice::confetti_ball:
---
+++ 00015_Unir con flechas 🔀/test.js
@@ -0,0 +1,22 @@
+// estas tres lineas son opcionales
+Muzzle.canvasHeight = 600;
+Muzzle.canvasWidth = 1200;
+Muzzle.pieceSize = 200;
+// Muzzle.strokeWidth = 0;
+
+const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-alternativa-kids/master/assets/attires/';
+
+
+// caso "unir con flechas"
+Muzzle.match([
+   `${baseUrl}/va_vacio.png`,
+   `${baseUrl}/cu_vacio.png`,
+   `${baseUrl}/chips_poco.png`
+ ], [
+   `${baseUrl}/va_fru.png`,
+   `${baseUrl}/cu_vai.png`,
+   `${baseUrl}/chips_mucho.png`,
+ ], [
+   `${baseUrl}/choc_mitad_vacio2.png` // opcional
+ ]);
+
