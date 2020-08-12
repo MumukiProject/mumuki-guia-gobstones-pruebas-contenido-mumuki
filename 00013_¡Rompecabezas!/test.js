91d4f8e1d0e2307cd488f3f8c46d258c7f942058
rocio@mumuki.org <rocio@mumuki.org>
2020-08-12 20:53:09 +0000

    Mumuki Export on 2020-08-12 20:53:09 +0000

--- 00013_¡Rompecabezas!/test.js
+++ 00013_¡Rompecabezas!/test.js
@@ -10,7 +10,9 @@ Muzzle.pieceSize = 50;
 
 const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-alternativa-kids/master/assets/attires/';
 
-Muzzle.basic(5, 5, 'https://flbulgarelli.github.io/headbreaker/static/berni.jpg')
+Muzzle.basic(5, 5, 'https://flbulgarelli.github.io/headbreaker/static/berni.jpg').then(canvas => {
+          canvas.borderFill(20);
+});
 
 
 
