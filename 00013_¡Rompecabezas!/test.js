55fb77dece1bb42fb3ac8e9b9cd077a46c29d2b2
rocio@mumuki.org <rocio@mumuki.org>
2020-08-12 21:41:32 +0000

    Mumuki Export on 2020-08-12 21:41:32 +0000

--- 00013_¡Rompecabezas!/test.js
+++ 00013_¡Rompecabezas!/test.js
@@ -5,7 +5,7 @@ $('#muzzle-canvas').css('margin-top', '15px');
 $('#muzzle-canvas').css('border-radius', '10px');
 $('#muzzle-canvas').css('width', '198%');
 $('.btn-submit-container').remove();
-$('a[href="/staging/exercises/10745-pruebas-contenido-mumuki-rompecabezas"]').css('content', '13');
+$('.progress-list-flex a::after').css('content', '13');
 
 // estas tres lineas son opcionales
 Muzzle.canvasHeight = 380;
