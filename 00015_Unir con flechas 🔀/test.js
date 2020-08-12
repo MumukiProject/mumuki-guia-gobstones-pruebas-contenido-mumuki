6824fac60cf659db9163bed8bcf85f14e4129a93
rocio@mumuki.org <rocio@mumuki.org>
2020-08-12 20:15:35 +0000

    Mumuki Export on 2020-08-12 20:15:35 +0000

--- 00015_Unir con flechas 🔀/test.js
+++ 00015_Unir con flechas 🔀/test.js
@@ -21,11 +21,11 @@ Muzzle.match([
  ]*/).then(canvas => {
           canvas.shuffle(0.5);
           canvas.puzzle.pieces.filter(it => !it.metadata.left).forEach(it => it.translate(600, 0))
-          canvas.puzzle.pieces[0].relocateTo(160, 60)
-          canvas.puzzle.pieces[2].relocateTo(160, 200)
-          canvas.puzzle.pieces[4].relocateTo(160, 340)
-          canvas.puzzle.pieces[1].relocateTo(460, 60)
-          canvas.puzzle.pieces[3].relocateTo(460, 200)
-          canvas.puzzle.pieces[5].relocateTo(460, 340)
+          canvas.puzzle.pieces[0].relocateTo(360, 60)
+          canvas.puzzle.pieces[2].relocateTo(360, 200)
+          canvas.puzzle.pieces[4].relocateTo(360, 340)
+          canvas.puzzle.pieces[1].relocateTo(560, 60)
+          canvas.puzzle.pieces[3].relocateTo(560, 200)
+          canvas.puzzle.pieces[5].relocateTo(560, 340)
           canvas.draw();
         });
