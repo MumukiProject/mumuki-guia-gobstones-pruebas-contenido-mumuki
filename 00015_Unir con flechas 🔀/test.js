22a6b2fcd1e9b3641dfb84c4dac0a5c5e404dc6e
rocio@mumuki.org <rocio@mumuki.org>
2020-08-12 23:05:50 +0000

    Mumuki Export on 2020-08-12 23:05:50 +0000

--- 00015_Unir con flechas 🔀/test.js
+++ 00015_Unir con flechas 🔀/test.js
@@ -1,5 +1,5 @@
 $('.capital-animation').remove(); 
-$('.mu-kids-character').append('<div position="left" margin-left="100px"><img src="https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-pruebas-contenido-mumuki/master/assets/luz_apagada-01_1597270246443.png" alt="luz_apagada-01_1597270246443.png" width="10%" height="10%" style = "margin-left: 90%;"></div>');
+$('.mu-kids-character').append('<div position="left" margin-left="100px"><img src="https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-pruebas-contenido-mumuki/master/assets/luz_apagada-01_1597270246443.png" alt="luz_apagada-01_1597270246443.png" width="15%" height="15%" style = "margin-left: 90%;"></div>');
 
 // estas tres lineas son opcionales
 Muzzle.canvasHeight = 500;
