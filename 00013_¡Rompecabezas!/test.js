1dd63326c616468d34bc3f02540d2afe5134d9b9
rocio@mumuki.org <rocio@mumuki.org>
2020-08-12 21:01:26 +0000

    Mumuki Export on 2020-08-12 21:01:26 +0000

--- 00013_¡Rompecabezas!/test.js
+++ 00013_¡Rompecabezas!/test.js
@@ -1,12 +1,12 @@
 mumuki.load(function() {
-  $('.mu-kids-state-image img').style.opacity = "0.3";
+  $('canvas').style.border = "thick solid black";
 });
 
 // estas tres lineas son opcionales
 Muzzle.canvasHeight = 380;
 Muzzle.canvasWidth = 600;
 Muzzle.pieceSize = 50;
-MuzzleCanvas.borderFill = 20;
+Canvas.borderFill = 20;
 // Muzzle.strokeWidth = 0;
 
 const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-alternativa-kids/master/assets/attires/';
