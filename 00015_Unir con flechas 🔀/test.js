25dde7067e8f6a44584bd69f37fb2d043d45d68d
rocio@mumuki.org <rocio@mumuki.org>
2020-08-12 23:04:28 +0000

    Mumuki Export on 2020-08-12 23:04:28 +0000

--- 00015_Unir con flechas 🔀/test.js
+++ 00015_Unir con flechas 🔀/test.js
@@ -1,4 +1,4 @@
-$('.capital-animation').remove();
+$('.capital-animation').remove(); 
 $('.mu-kids-character').append('<div position="left" margin-left="100px"><img src="https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-pruebas-contenido-mumuki/master/assets/luz_apagada-01_1597270246443.png" alt="luz_apagada-01_1597270246443.png" width="10%" height="10%" style = "margin-left: 90%;"></div>');
 
 // estas tres lineas son opcionales
