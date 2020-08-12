56078c86cf0fc9d464f51b2e83f4f30e090c55f5
rocio@mumuki.org <rocio@mumuki.org>
2020-08-12 18:10:07 +0000

    Mumuki Export on 2020-08-12 18:10:07 +0000

---
+++ 00016_Multiplechoice/test.js
@@ -0,0 +1,22 @@
+// estas tres lineas son opcionales
+Muzzle.canvasHeight = 600;
+Muzzle.canvasWidth = 1200;
+Muzzle.pieceSize = 200;
+// Muzzle.strokeWidth = 0;
+
+const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-alternativa-kids/master/assets/attires/';
+
+
+// caso "choice"
+Muzzle.match([
+  `${baseUrl}/va_vacio.png`
+], [
+  `${baseUrl}/va_fru.png`,
+], [
+  `${baseUrl}/cu_vai.png`,
+  `${baseUrl}/choc_mitad_vacio2.png`
+]).then(canvas => {
+          canvas.shuffle(0.4);
+          canvas.puzzle.pieces.filter(it => !it.metadata.left).forEach(it => it.translate(400, 0))
+          canvas.draw();
+        });
