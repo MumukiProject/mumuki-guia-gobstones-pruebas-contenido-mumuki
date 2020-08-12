f3a52f42e2c72b2c59519bc6d19dcb93992612e1
rocio@mumuki.org <rocio@mumuki.org>
2020-08-12 21:34:21 +0000

    Mumuki Export on 2020-08-12 21:34:21 +0000

--- 00013_¡Rompecabezas!/test.js
+++ 00013_¡Rompecabezas!/test.js
@@ -1,7 +1,7 @@
 $('.mu-kids-state-image').css('opacity', '0.5');
 $('#muzzle-canvas').css('border', '5px');
 $('#muzzle-canvas').css('border-style', 'solid');
-$('#muzzle-canvas').css('margin-top', '10px');
+$('#muzzle-canvas').css('margin-top', '15px');
 $('.btn-submit-container').remove();
 $('.progress-list-flex a.active:after').css('content', '13');
 
