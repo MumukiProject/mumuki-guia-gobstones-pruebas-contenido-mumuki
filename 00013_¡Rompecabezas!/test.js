10131ae84e03902676db7d22bc93aa36e8fe3aeb
rocio@mumuki.org <rocio@mumuki.org>
2020-08-12 20:33:01 +0000

    Mumuki Export on 2020-08-12 20:33:00 +0000

--- 00013_¡Rompecabezas!/test.js
+++ 00013_¡Rompecabezas!/test.js
@@ -9,7 +9,7 @@ const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gob
 Muzzle.basic(5, 5, 'https://flbulgarelli.github.io/headbreaker/static/berni.jpg');
 
 mumuki.load(function() {
-$('mu-kids-state-image').remove;
+  $('mu-kids-state-image').remove;
 }
 // caso "choice"
 //Muzzle.match([
