200656c4c805a1595868b95c114d64b01751dfea
rocio@mumuki.org <rocio@mumuki.org>
2020-08-12 23:36:02 +0000

    Mumuki Export on 2020-08-12 23:36:02 +0000

--- 00016_Multiplechoice/test.js
+++ 00016_Multiplechoice/test.js
@@ -6,7 +6,7 @@ $('.mu-kids-exercise .mu-kids-character .mu-kids-character-speech-bubble').css('
 $('.mu-kids-character').append('<div position="left" margin-left="100px"><img src="https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-pruebas-contenido-mumuki/master/assets/luz_encendida-01_1597270985305.png" alt="luz_apagada-01_1597270246443.png" width="10%" height="10%" style = "margin-left: 90%;" onclick="open()"><img src="https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-pruebas-contenido-mumuki/master/assets/choice_1597255722913.gif" alt="luz_apagada-01_1597270246443.png" width="50%" height="50%" style = "margin-left: 90%; display: none;" onclick="close()"</div>');
 
 function open() {
-  $('img[src="https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-pruebas-contenido-mumuki/master/assets/choice_1597255722913.gif"]').css('display', 'block')
+  $('img[src="https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-pruebas-contenido-mumuki/master/assets/choice_1597255722913.gif"]').css('display', 'inline')
 }
 function close() {
   $('img[src="https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-pruebas-contenido-mumuki/master/assets/choice_1597255722913.gif"]').css('display', 'none')
