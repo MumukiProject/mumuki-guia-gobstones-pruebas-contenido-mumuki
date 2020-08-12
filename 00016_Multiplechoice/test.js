b510705ee6283e3435d976a5f6462a86a71ed746
rocio@mumuki.org <rocio@mumuki.org>
2020-08-12 22:20:38 +0000

    Mumuki Export on 2020-08-12 22:20:38 +0000

--- 00016_Multiplechoice/test.js
+++ 00016_Multiplechoice/test.js
@@ -3,7 +3,7 @@ $('#muzzle-canvas').css('border-style', 'solid');
 $('#muzzle-canvas').css('margin-top', '15px');
 $('#muzzle-canvas').css('border-radius', '10px');
 $('.mu-kids-exercise .mu-kids-character .mu-kids-character-speech-bubble').css('width', '50%');
-$('.mu-kids-character').append('<div position="left" margin-left="100px"><img src="https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-pruebas-contenido-mumuki/master/assets/luz_apagada-01_1597270246443.png" alt="luz_apagada-01_1597270246443.png" width="10%" height="10%" style = "margin-left='90%'"></div>');
+$('.mu-kids-character').append('<div position="left" margin-left="100px"><img src="https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gobstones-pruebas-contenido-mumuki/master/assets/luz_apagada-01_1597270246443.png" alt="luz_apagada-01_1597270246443.png" width="10%" height="10%" style = "margin-left: 90%;"></div>');
 
 // estas tres lineas son opcionales
 Muzzle.canvasHeight = 450;
