7fb5ce0b983ae514da7994c0d574ca804387e301
rocio@mumuki.org <rocio@mumuki.org>
2020-11-03 18:22:16 +0000

    Mumuki Export on 2020-11-03 18:22:16 +0000

--- 00023_Unir con flechas para ver con Moni/test.js
+++ 00023_Unir con flechas para ver con Moni/test.js
@@ -3,7 +3,7 @@ const baseUrl = 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-gob
 Muzzle.match([
   `${baseUrl}/match_1_1_1604427324928.png`
 ], [
-  `${baseUrl}/match_1_2_1604427336907.png`,
-], [
   `${baseUrl}/match_1_1604427304341.png`,
+], [
+  `${baseUrl}/match_1_2_1604427336907.png`,
 ])
